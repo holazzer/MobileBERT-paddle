ed523c71e63fa374a9a34680cf927e5dcb8f2ff2
holazzer <751727648@qq.com>
2021-09-05 09:45:55 +0800

    fix prepare (finally!)

--- finetune/squad_util.py
+++ finetune/squad_util.py
@@ -9,37 +9,66 @@ from paddlenlp.datasets import load_dataset
 
 from paddle.io import Dataset
 
+# hacks for MobileBertTokenizer
+CLS_TOKEN = '[CLS]'
+SEP_TOKEN = '[SEP]'
+PAD_TOKEN = '[PAD]'
 
-def prepare(example, tokenizer: BertTokenizer):
+
+def prepare(example, tokenizer: 'BertTokenizer', max_sq=512):
     context: str = example['context']
+    c = tokenizer.tokenize(context)
+
     question: str = example['question']
+    q = tokenizer.tokenize(question)
+
     answers: List[str] = example['answers']
     answers_starts: List[int] = example['answer_starts']
     answers_ends = [i + len(ans) for i, ans in zip(answers_starts, answers)]
+
+    tk_level_starts = []
+    tk_level_ends = []
+
     is_impossible: bool = example['is_impossible']
 
     if is_impossible or len(answers) == 0:
         labels = [[0, 0]] * 4
     else:
-        head = min(answers_starts)
-        tail = max(answers_ends)
-        tol = 510 - len(question)
+
+        # char-level start -> token-level start
+        for ans, char_start in zip(answers, answers_starts):
+            b4 = context[:char_start]
+            tk_start = len(tokenizer.tokenize(b4))
+            tk_level_starts.append(tk_start + 1)  # add the [CLS] token ahead
+            tk_len = len(tokenizer.tokenize(ans))
+            tk_level_ends.append(tk_start + tk_len)
+
+        # see if we need to move back the start
+        head = min(tk_level_starts)
+        tail = max(tk_level_ends)
+        tol = max_sq - 3 - len(q)  # tokens left after removing the question
 
         if tail > tol:  # need offset
             offset = tail - tol
-            new_head = (head - offset)//2
+            new_head = (head - offset) // 2
             offset = offset + new_head
             for i in range(len(answers)):
                 answers_starts[i] -= offset
                 answers_ends[i] -= offset
+            c = c[offset:]
+        else:
+            c = c[:tol]
 
-        labels = [[st, ed] for st, ed in zip(answers_starts, answers_ends)]
+        labels = [[st, ed] for st, ed in zip(tk_level_starts, tk_level_ends)]
         if len(labels) < 4: labels = (labels * 4)[:4]
 
-    tkd: dict = tokenizer(context, question, max_seq_len=512,
-                          pad_to_max_seq_len=True)
-    input_ids = paddle.to_tensor(tkd['input_ids'])
-    token_type_ids = paddle.to_tensor(tkd['token_type_ids'])
+    input_ids = [CLS_TOKEN] + c + [SEP_TOKEN] + q + [SEP_TOKEN]
+    pad_cnt = (max_sq - len(input_ids))
+    input_ids += [PAD_TOKEN] * pad_cnt
+    input_ids = tokenizer.convert_tokens_to_ids(input_ids)
+    input_ids = np.array(input_ids)
+    token_type_ids = [0] * (len(c)+2) + [1] * (len(q)+1) + [0] * pad_cnt
+    token_type_ids = np.array(token_type_ids)
 
     return input_ids, token_type_ids, np.array(labels)
 
